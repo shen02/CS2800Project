cc17be322633d1eca5be153cacf2ab29b2a533ef
angelashen <shen.ang@northeastern.edu>
2020-11-08 22:51:34 -0500

    Accepted revisions

--- src/LightsOutSolverMain.java
+++ src/LightsOutSolverMain.java
@@ -4,34 +4,37 @@ import java.util.Scanner;
 public class LightsOutSolverMain {
 
   /**
-   * Main method of LightsOutSolver. Prompts for user inputs and returns the appropriate solution.
+   * Main method of {@link LightsOutSolver}. Prompts two user inputs n and k representing the
+   * dimension of the game's square board and the number of moves in which to win the game
+   * respectively. Both inputs must be positive integers.
    *
-   * @param args Unused argument.
+   * @param args Arguments from stdin. Never used.
    */
   public static void main(String[] args) {
     Scanner c = new Scanner(System.in);
     LightsOutSolver solver = new LightsOutSolver();
-    String promptDim = "Please enter a positive dimension or 0 to quit: ";
-    String promptTurn = "Please enter a positive # of turns or 0 to quit: ";
+    String promptDimension = "Please enter the dimension of the square board, or 0 to quit: ";
+    String promptNumTurns = "Please enter the number of turns to win in, or 0 to quit: ";
 
-    System.out.println("Welcome to Lights-out SAT-Solver.");
-    System.out.print(promptDim);
+    System.out.println("Welcome to the Lights Out SAT Solver.");
+    System.out.print(promptDimension);
     int n = c.nextInt();
     int k;
+
     while (n != 0) {
-      System.out.print(promptTurn);
+      System.out.print(promptNumTurns);
       k = c.nextInt();
       if (k == 0) {
         break;
       }
       try {
         solver.sat(new Context(), n, k + 1);
-      } catch (IllegalArgumentException e) {
-        System.out.println("Please enter positive values only.");
+      } catch (IllegalArgumentException ie) {
+        System.out.println(ie.getMessage());
       }
       System.out.print("Please enter a dimension or 0 to quit: ");
       n = c.nextInt();
     }
-    System.out.print("Goodbye.");
+    System.out.print("Exited from Lights Out SAT Solver. Goodbye.");
   }
 }
